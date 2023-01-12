3975eca806ca8e7a9b10c1f7c7cfa2a8ca2b0cf8
Jean-Pierre <jpphi@yahoo.fr>
2023-01-12 16:04:01 +0100

    Ajout Booléen Fin de partie

--- Assets/Script/GestionPartie.cs
+++ Assets/Script/GestionPartie.cs
@@ -11,7 +11,7 @@ public class GestionPartie : MonoBehaviour
 
     private int indicePion;
     //Vector3 pos;
-    bool ligneComplete;
+    bool ligneComplete, FinPartie;
     private int[] CodeCouleur = new int[Globales.NB_PION_LIGNE];
 
 
@@ -19,48 +19,51 @@ public class GestionPartie : MonoBehaviour
     {
         // Initialisation de La partie
         init_partie();
-        ligneComplete = false;
+        ligneComplete = FinPartie= false;
 
     }
             
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.T))
+        if (!FinPartie)
         {
-            code.Triche();
-        }
-        else if (Input.GetKeyDown(KeyCode.R))
-        {
-            ligneComplete= ligne.Ajoute_Pion_Ligne(Globales.RED_COLOR);
-        }
 
-        else if (Input.GetKeyDown(KeyCode.B))
-        {
-            ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.BLUE_COLOR);
-        }
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                code.Triche();
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.RED_COLOR);
+            }
 
-        else if (Input.GetKeyDown(KeyCode.Y))
-        {
-            ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.YELLOW_COLOR);
-        }
+            else if (Input.GetKeyDown(KeyCode.B))
+            {
+                ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.BLUE_COLOR);
+            }
 
-        else if (Input.GetKeyDown(KeyCode.P))
-        {
-            ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.PURPLE_COLOR);
-        }
+            else if (Input.GetKeyDown(KeyCode.Y))
+            {
+                ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.YELLOW_COLOR);
+            }
 
-        else if (Input.GetKeyDown(KeyCode.C))
-        {
-            ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.CYAN_COLOR);
-        }
+            else if (Input.GetKeyDown(KeyCode.P))
+            {
+                ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.PURPLE_COLOR);
+            }
 
-        else if (Input.GetKeyDown(KeyCode.G))
-        {
-            ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.GREEN_COLOR);
-        }
+            else if (Input.GetKeyDown(KeyCode.C))
+            {
+                ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.CYAN_COLOR);
+            }
 
-        //Debug.Log("Booleen ligneComplete" + ligneComplete);
+            else if (Input.GetKeyDown(KeyCode.G))
+            {
+                ligneComplete = ligne.Ajoute_Pion_Ligne(Globales.GREEN_COLOR);
+            }
+
+            //Debug.Log("Booleen ligneComplete" + ligneComplete);
+        }
 
     }
 
@@ -88,8 +91,8 @@ public class GestionPartie : MonoBehaviour
 
     public void OnAGagne() // Déclenché dans Ligne lorsque 4 marques noires sont donnée 
     {
-
         Debug.Log("On a Gagne !!!! " + Globales.SCORE);
+        FinPartie = true;
     }
 
 
