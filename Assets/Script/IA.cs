ed581306a866ed1bf721993daa7fa8c7d877bf10
Jean-Pierre <jpphi@yahoo.fr>
2023-01-23 10:51:57 +0100

    Certains packages rendais le build impossible. Réglé

--- Assets/Script/IA.cs
+++ Assets/Script/IA.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class IA : MonoBehaviour
